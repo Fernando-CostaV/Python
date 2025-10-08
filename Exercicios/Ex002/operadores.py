6f5f36797dad6d1e6e053b7cc90b0d54e8b76d26
Fernando-CostaV <fernando_costa45@hotmail.com>
2025-10-08 10:29:22 -0300

    novo exercicio

---
+++ Exercicios/Ex002/operadores.py
@@ -0,0 +1,18 @@
+a=10
+b=3
+
+soma = a + b # 13
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+divisao_inteira = a // b
+modulo = a % b
+exponeciacao = a ** b
+
+print(f"soma {soma}")
+print(f"subtracao{subtracao}")
+print(f"multiplicacao{multiplicacao}")
+print(f"divisao{divisao}")  
+print(f"divisao_inteira{divisao_inteira}")
+print(f"modulo{modulo}")
+print(f"exponeciacao{exponeciacao}")
