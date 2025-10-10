1b0c63e0d262d2c0e573e11e3935ed6f66a58613
Fernando-CostaV <fernando_costa45@hotmail.com>
2025-10-10 10:25:47 -0300

    continuando exercício

--- Exercicios/Ex002/operadores.py
+++ Exercicios/Ex002/operadores.py
@@ -16,3 +16,20 @@ print(f"divisao{divisao}")
 print(f"divisao_inteira{divisao_inteira}")
 print(f"modulo{modulo}")
 print(f"exponeciacao{exponeciacao}")
+
+a=10
+b=3
+
+igual = a == b
+diferente = a != b
+maior = a > b
+menor = a < b
+maior_igual = a >= b
+menor_igual = a <= b
+
+print(f"igual{igual}")
+print(f"diferente{diferente}")
+print(f"maior{maior}")
+print(f"menor{menor}")
+print(f"maior_igual{maior_igual}")
+print(f"menor_igual{menor_igual}")
