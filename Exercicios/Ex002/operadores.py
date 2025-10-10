3660d2c12e62e1d561a3bd6e92cb999f4cd2d54d
Fernando-CostaV <fernando_costa45@hotmail.com>
2025-10-10 20:54:19 -0300

    Update operadore.py

--- Exercicios/Ex002/operadores.py
+++ Exercicios/Ex002/operadores.py
@@ -1,13 +1,14 @@
-a=10
+#Aritmeticos
+a=10 
 b=3
 
-soma = a + b # 13
-subtracao = a - b
-multiplicacao = a * b
-divisao = a / b
-divisao_inteira = a // b
-modulo = a % b
-exponeciacao = a ** b
+soma = a + b # Soma (+): soma dois valores
+subtracao = a - b  #Subtração (-): subtrai o segundo valor do primeiro
+multiplicacao = a * b # Multiplicação (*): multiplica dois valores
+divisao = a / b # Divisão (/): divide o primeiro valor pelo segundo e devolve um resultado de tipo flutuante
+divisao_inteira = a // b # Divisão inteira (//): divide o primeiro valor pelo segundo e devolve um resultado de tipo inteiro (a parte decimal é descartada).
+modulo = a % b # Módulo (%): devolve o resto da divisão entre o primeiro valor e o segundo
+exponeciacao = a ** b # Exponenciação (**): eleva o primeiro valor à potência do segundo
 
 print(f"soma {soma}")
 print(f"subtracao{subtracao}")
@@ -17,15 +18,16 @@ print(f"divisao_inteira{divisao_inteira}")
 print(f"modulo{modulo}")
 print(f"exponeciacao{exponeciacao}")
 
+# De comparação
 a=10
 b=3
 
-igual = a == b
-diferente = a != b
-maior = a > b
-menor = a < b
-maior_igual = a >= b
-menor_igual = a <= b
+igual = a == b #Igual a (==): devolve True se ambos os valores são iguais
+diferente = a != b # Diferente de (!=): devolve True se os valores são diferentes
+maior = a > b #Maior que (>): devolve True se o primeiro valor é maior que o segundo
+menor = a < b # Menor que (<): devolve True se o primeiro valor é menor que o segundo
+maior_igual = a >= b # Maior ou igual que (>=): devolve True se o primeiro valor é maior ou igual que o segundo
+menor_igual = a <= b # Menor ou igual que (<=): devolve True se o primeiro valor é menor ou igual que o segundo
 
 print(f"igual{igual}")
 print(f"diferente{diferente}")
@@ -33,3 +35,15 @@ print(f"maior{maior}")
 print(f"menor{menor}")
 print(f"maior_igual{maior_igual}")
 print(f"menor_igual{menor_igual}")
+
+#Logicos
+a=10
+b=3
+
+resultado_and = (a > 5) and (b <5) # AND (and): devolve True se ambas as expressões forem verdadeiras
+resultado_or = (a >15) or (b < 5) # OR (or): devolve True se pelo menos uma das expressões for verdadeira
+resultado_not = not( a > 5) # NOT (not): inverte o valor lógico da expressão (True vira False e vice-versa)
+
+print(f"reultado_ando{resultado_and}")
+print(f"resultado_or{resultado_or}")
+print(f"resultado_not{resultado_not}")
