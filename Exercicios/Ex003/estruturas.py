974f5be56aa91bfa478b66373fef33d6f4211f68
Fernando-CostaV <fernando_costa45@hotmail.com>
2025-10-10 22:10:03 -0300

    Ex003

---
+++ Exercicios/Ex003/estruturas.py
@@ -0,0 +1,27 @@
+idade = 18
+
+if idade >= 18:
+    print("Você é maior de idade.")
+     #A estrutura if é utilizada para executar um bloco de código se uma condição for verdadeira. A sintaxe básica é a seguinte
+
+idade = 15
+
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
+#A estrutura if-else nos permite especificar um bloco de código alternativo que será executado se a condição do if for falsa
+
+nota= 85
+
+if nota >= 90:
+    print ("Excelente")
+
+elif nota >= 80:
+   print ("Muito bom")
+
+elif nota >= 70:
+   print ("Bom")
+
+else:
+   print ("Precisa melhorar")   
