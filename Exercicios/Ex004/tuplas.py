3e48fcae0eaaa20b208ad4d82af44fed2e789384
Fernando-CostaV <fernando_costa45@hotmail.com>
2025-10-14 23:01:39 -0300

    aula_4.1

---
+++ Exercicios/Ex004/tuplas.py
@@ -0,0 +1,12 @@
+#Criação de acesso
+ponto = (3,4)
+
+print(ponto[0])
+print(ponto[1])
+
+print("\nMetodos de tuplas")
+minha_tupla = (1,2,3,2,4,2)
+
+print ( minha_tupla.index(2))
+print (minha_tupla.index(2,2))
+print (minha_tupla.index(2,2,4))
