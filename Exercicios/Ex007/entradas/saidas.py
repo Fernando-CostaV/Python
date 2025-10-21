4829786ffbefea12ec0d74cb02c60ed856cd70dd
Fernando-CostaV <fernando_costa45@hotmail.com>
2025-10-21 08:18:41 -0300

    att

---
+++ Exercicios/Ex007/entradas/saidas.py
@@ -0,0 +1,14 @@
+print("\nEntradas de dados")
+nome = input("Fernando:")
+idade = input("28:")
+
+print("Olá", "Fernando", "!")
+print("Você tem", "28", "anos.")
+
+
+idade = int(input("28:"))
+
+if idade >=18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
